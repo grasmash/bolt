540865da59022e1e38c3606aa97d042e49236a07
Matthew Grasmick <matt.grasmick@gmail.com>
2017-02-06 12:46:54 -0500

    Updating executor calls.

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -129,7 +129,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
     if ($this->isInitialInstall()) {
       $this->io->write('<info>Creating BLT templated files...</info>');
       // The BLT command will not work at this point because the .git dir doesn't exist yet.
-      $success = $this->executeCommand($this->getVendorPath() . '/acquia/blt/blt.sh create-project', [], TRUE);
+      $success = $this->executeCommand($this->getVendorPath() . '/acquia/blt/bin/blt create-project', [], TRUE);
     }
     elseif ($options['blt']['update']) {
       $this->io->write('<info>Updating BLT templated files...</info>');
